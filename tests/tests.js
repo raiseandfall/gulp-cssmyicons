29a0409a7895c8086c2f7e3fee13b6e72ef05a00
raiseandfall <druidev+github@gmail.com>
2015-10-01 17:17:38 -0700

    [TEST] Added empty tests

---
+++ tests/tests.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var cssMyIcons = require('../');
+var expect = require('expect.js');
+var fs = require('fs');
+var path = require('path');
+
+describe('gulp cssmyicons', function() {
+
+  it('should generate a CSS file', function() {
+
+  });
+
+  it('should generate a CSS file in a destination directory', function() {
+
+  });
+
+  it('should generate icons path with a prefix to the path', function() {
+
+  });
+
+});
