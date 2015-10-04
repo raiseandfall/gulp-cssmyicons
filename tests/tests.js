7b55fac23aa4f05633be1f4d379a411f4f33f0c4
raiseandfall <druidev+github@gmail.com>
2015-10-03 23:01:12 -0700

    [TEST] Added two tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,18 +1,45 @@
 'use strict';
 
+var gulp = require('gulp');
 var cssMyIcons = require('../');
 var assert = require('assert');
 var fs = require('fs');
-var path = require('path');
+var es = require('event-stream');
 
 describe('gulp cssmyicons', function() {
 
-  it('should generate a CSS file', function(cb) {
-    
+  it('should generate a CSS file', function(done) {
+    gulp.src(__dirname + '/fixtures/*.svg')
+      .pipe(cssMyIcons('icons.css').on('error', function(err) {
+        console.log('err:', err);
+      }))
+      .pipe(gulp.dest('./'))
+      .pipe(es.wait(function() {
+        assert.equal(
+          fs.readFileSync('icons.css', 'utf8'),
+          fs.readFileSync('tests/mocks/icons.css', 'utf8')
+        );
+
+        fs.unlinkSync('icons.css');
+        done();
+      }));
   });
 
-  it('should generate a CSS file in a destination directory', function() {
+  it('should generate a CSS file and move it to a destination directory', function() {
+    gulp.src(__dirname + '/fixtures/*.svg')
+      .pipe(cssMyIcons('icons.css').on('error', function(err) {
+        console.log('err:', err);
+      }))
+      .pipe(gulp.dest('tests/'))
+      .pipe(es.wait(function() {
+        assert.equal(
+          fs.readFileSync('tests/icons.css', 'utf8'),
+          fs.readFileSync('tests/mocks/icons.css', 'utf8')
+        );
 
+        fs.unlinkSync('tests/icons.css');
+        done();
+      }));
   });
 
   it('should generate icons path with a prefix to the path', function() {
